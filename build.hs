00f2316b54675ba84eb6add13be94563e14145a6
Etienne Millon <me@emillon.org>
2014-03-18 15:28:46 +0100

    merge tests together

--- build.hs
+++
@@ -1,7 +0,0 @@
-import System.Cmd
-import System.Exit
-
-main :: IO ()
-main = do
-    status <- system "./dist/build/blog/blog build"
-    exitWith status
--- checklinks.hs
+++
@@ -1,7 +0,0 @@
-import System.Cmd
-import System.Exit
-
-main :: IO ()
-main = do
-    status <- system "linkchecker --check-html --check-css --anchors _site/index.html"
-    exitWith status
