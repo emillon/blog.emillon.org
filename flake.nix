ca62a6f589074d41aa95a9ff33bb42ae60132884
Etienne Millon <me@emillon.org>
2024-04-17 11:22:07 +0200

    build contents as derivation

--- flake.nix
+++ flake.nix
@@ -2,15 +2,23 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs";
     flake-utils.url = "github:numtide/flake-utils";
+    nix-filter.url = "github:numtide/nix-filter";
   };
-  outputs = { self, nixpkgs, flake-utils }:
+  outputs = { self, nixpkgs, flake-utils, nix-filter }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
-        haskellPackages = pkgs.haskellPackages;
-        packageName = "blog";
+        blogEngine = pkgs.haskellPackages.callCabal2nix "blog" self rec { };
       in {
-        packages.default =
-          haskellPackages.callCabal2nix packageName self rec { };
+        packages.default = pkgs.stdenv.mkDerivation {
+          name = "blog-contents";
+          nativeBuildInputs = [ blogEngine ];
+          src = nix-filter {
+            root = ./.;
+            include = [ "posts" "static" "templates" ];
+          };
+          buildPhase = "LC_ALL=C.UTF-8 blog build";
+          installPhase = "cp -r _site/. $out/";
+        };
       });
 }
