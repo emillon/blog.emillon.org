aa3484225a4498bafc082a90a61ce0dc0ac90d03
Etienne Millon <me@emillon.org>
2024-04-23 13:58:23 +0200

    dry run

--- blog.ml
+++ blog.ml
@@ -2,19 +2,52 @@ module Cmdliner_let_syntax = struct
   let ( let+ ) x f =
     let open Cmdliner.Term in
     const f $ x
+
+  let pair x y = (x, y)
+
+  let ( and+ ) x y =
+    let open Cmdliner.Term in
+    const pair $ x $ y
 end
+
+type ops = {
+  mkdir : string -> unit;
+  create_file : path:string -> contents:string -> unit;
+}
+
+let real_ops ~root =
+  let mkdir path = Sys.mkdir (Filename.concat root path) 0o755 in
+  let create_file ~path ~contents =
+    Out_channel.with_open_bin (Filename.concat root path) (fun oc ->
+        Out_channel.output_string oc contents)
+  in
+  { mkdir; create_file }
+
+let dry_run =
+  let mkdir path = Printf.printf "mkdir %S\n" path in
+  let create_file ~path ~contents =
+    let hash = Digest.string contents |> Digest.to_hex in
+    Printf.printf "create_file %S (hash: %s)\n" path hash
+  in
+  { mkdir; create_file }
+
+let run ~input:_ ~output =
+  let ops = match output with None -> dry_run | Some root -> real_ops ~root in
+  ops.mkdir ".";
+  ops.create_file ~path:"test" ~contents:"hello\n"
 
 let info = Cmdliner.Cmd.info "blog"
 
 let term =
   let open Cmdliner_let_syntax in
-  let+ output =
+  let+ input =
     let open Cmdliner.Arg in
-    required & opt (some string) None & info [ "o" ]
+    required & opt (some string) None & info [ "i" ]
+  and+ output =
+    let open Cmdliner.Arg in
+    value & opt (some string) None & info [ "o" ]
   in
-  Sys.mkdir output 0o755;
-  Out_channel.with_open_bin (Filename.concat output "test") (fun oc ->
-      Out_channel.output_string oc "hello\n")
+  run ~input ~output
 
 let cmd = Cmdliner.Cmd.v info term
 let () = Cmdliner.Cmd.eval cmd |> Stdlib.exit
---
+++ blog.t
@@ -0,0 +1,3 @@
+  $ ./blog.exe -i .
+  mkdir "."
+  create_file "test" (hash: b1946ac92492d2347c6235b4d2611184)
