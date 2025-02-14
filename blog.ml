0bb7c3e5402bbc6652d5099ca593d54bb6a86cbd
Etienne Millon <me@emillon.org>
2025-02-14 17:10:58 +0100

    post module

--- blog.ml
+++ blog.ml
@@ -3,10 +3,62 @@ open Stdio
 
 let ( // ) = Stdlib.Filename.concat
 
+let with_string_formatter f =
+  let buf = Buffer.create 0 in
+  let fmt = Stdlib.Format.formatter_of_buffer buf in
+  f fmt;
+  Buffer.contents buf
+
+let render_template input l =
+  let env = Map.of_alist_exn (module String) l in
+  with_string_formatter (fun fmt -> Template.process env ~input ~output:fmt)
+
+let render_default ~title ~body =
+  render_template "templates/default.html" [ ("title", title); ("body", body) ]
+
 let readdir path =
   Stdlib.Sys.readdir path |> Array.to_list
   |> List.sort ~compare:String.compare
   |> List.map ~f:(fun base -> path // base)
+
+let directory_exists path =
+  Stdlib.Sys.file_exists path && Stdlib.Sys.is_directory path
+
+let weekday_to_string = function
+  | `Mon -> "Mon"
+  | `Tue -> "Tue"
+  | `Wed -> "Wed"
+  | `Thu -> "Thu"
+  | `Fri -> "Fri"
+  | `Sat -> "Sat"
+  | `Sun -> "Sun"
+
+let month_name = function
+  | 1 -> "Jan"
+  | 2 -> "Feb"
+  | 3 -> "Mar"
+  | 4 -> "Apr"
+  | 5 -> "May"
+  | 6 -> "Jun"
+  | 7 -> "Jul"
+  | 8 -> "Aug"
+  | 9 -> "Sep"
+  | 10 -> "Oct"
+  | 11 -> "Nov"
+  | 12 -> "Dec"
+  | n -> Printf.ksprintf invalid_arg "month_name: %d" n
+
+let date_to_rss_string date =
+  let ptime = Ptime.of_date date |> Option.value_exn in
+  let y, m, d = date in
+  let weekday = Ptime.weekday ptime in
+  Printf.sprintf "%s, %d %s %d 00:00:00 UT"
+    (weekday_to_string weekday)
+    d (month_name m) y
+
+let date_to_short_string date =
+  let y, m, d = date in
+  Printf.sprintf "%d %s %d" d (month_name m) y
 
 module Ops = struct
   type t = {
@@ -51,35 +103,6 @@ module Ops = struct
     { mkdir; create_file }
 end
 
-type post = {
-  basename : string;
-  tags : Set.M(String).t;
-  title : string;
-  author : string;
-  body : Cmarkit.Doc.t;
-}
-
-let load_post path =
-  let contents = In_channel.read_all path in
-  let yaml = Yaml.of_string_exn contents in
-  let basename = Stdlib.Filename.basename path in
-  let tags =
-    Yaml.Util.find_exn "tags" yaml
-    |> Option.value_exn |> Yaml.Util.to_string_exn |> String.split ~on:','
-    |> List.map ~f:String.strip
-    |> Set.of_list (module String)
-  in
-  let title =
-    Yaml.Util.find_exn "title" yaml
-    |> Option.value_exn |> Yaml.Util.to_string_exn
-  in
-  let author =
-    Yaml.Util.find_exn "author" yaml
-    |> Option.value_exn |> Yaml.Util.to_string_exn
-  in
-  let body = Cmarkit.Doc.of_string contents in
-  { basename; tags; title; author; body }
-
 type feed_config = {
   title : string;
   description : string;
@@ -88,71 +111,95 @@ type feed_config = {
   root : Uri.t;
 }
 
-let load_all_posts path =
-  readdir path
-  |> List.filter ~f:(fun s ->
-         Stdlib.Filename.check_suffix s ".mdwn"
-         || Stdlib.Filename.check_suffix s ".lhs")
-  |> List.map ~f:load_post
+module Post = struct
+  type t = {
+    basename : string;
+    tags : Set.M(String).t;
+    title : string;
+    author : string;
+    body : Cmarkit.Doc.t;
+  }
 
-let post_permalink _post = "permalink"
-let post_rss_description _post = "description"
+  let load path =
+    let contents = In_channel.read_all path in
+    let yaml = Yaml.of_string_exn contents in
+    let basename = Stdlib.Filename.basename path in
+    let tags =
+      Yaml.Util.find_exn "tags" yaml
+      |> Option.value_exn |> Yaml.Util.to_string_exn |> String.split ~on:','
+      |> List.map ~f:String.strip
+      |> Set.of_list (module String)
+    in
+    let title =
+      Yaml.Util.find_exn "title" yaml
+      |> Option.value_exn |> Yaml.Util.to_string_exn
+    in
+    let author =
+      Yaml.Util.find_exn "author" yaml
+      |> Option.value_exn |> Yaml.Util.to_string_exn
+    in
+    let body = Cmarkit.Doc.of_string contents in
+    { basename; tags; title; author; body }
 
-let post_pubdate post =
-  Stdlib.Scanf.sscanf post.basename "%d-%d-%d" (fun y m d -> (y, m, d))
+  let load_all path =
+    readdir path
+    |> List.filter ~f:(fun s ->
+           Stdlib.Filename.check_suffix s ".mdwn"
+           || Stdlib.Filename.check_suffix s ".lhs")
+    |> List.map ~f:load
 
-let weekday_to_string = function
-  | `Mon -> "Mon"
-  | `Tue -> "Tue"
-  | `Wed -> "Wed"
-  | `Thu -> "Thu"
-  | `Fri -> "Fri"
-  | `Sat -> "Sat"
-  | `Sun -> "Sun"
+  let pub_date post =
+    Stdlib.Scanf.sscanf post.basename "%d-%d-%d" (fun y m d -> (y, m, d))
 
-let month_name = function
-  | 1 -> "Jan"
-  | 2 -> "Feb"
-  | 3 -> "Mar"
-  | 4 -> "Apr"
-  | 5 -> "May"
-  | 6 -> "Jun"
-  | 7 -> "Jul"
-  | 8 -> "Aug"
-  | 9 -> "Sep"
-  | 10 -> "Oct"
-  | 11 -> "Nov"
-  | 12 -> "Dec"
-  | n -> Printf.ksprintf invalid_arg "month_name: %d" n
+  (* TODO *)
+  let permalink _post = "permalink"
+
+  (* TODO *)
+  let rss_description _post = "description"
+
+  let to_rss_item post ~config =
+    let open Tyxml.Xml in
+    node "item"
+      [
+        node "title" [ pcdata post.title ];
+        node "link" [ pcdata (permalink post) ];
+        node "description" [ cdata (rss_description post) ];
+        node "pubDate" [ pcdata (date_to_rss_string (pub_date post)) ];
+        node "guid" [ pcdata (permalink post) ];
+        node "dc:creator" [ pcdata config.author_name ];
+      ]
+
+  let create_assets ops path =
+    Ops.mkdir ops path;
+    List.iter (readdir path) ~f:(fun path ->
+        Ops.create_file ops ~path ~contents:(In_channel.read_all path))
+
+  let render post =
+    let title = post.title in
+    let body =
+      render_template "templates/post.html"
+        [
+          ("title", title);
+          ("author", post.author);
+          ("date", date_to_short_string (pub_date post));
+          ("prettytags", String.concat ~sep:", " (Set.to_list post.tags));
+          ("body", Cmarkit_html.of_doc ~safe:true post.body);
+        ]
+    in
+    render_default ~title ~body
+
+  let create ops post =
+    let chopped = "posts" // post.basename |> Stdlib.Filename.chop_extension in
+    let path = chopped ^ ".html" in
+    if directory_exists chopped then create_assets ops chopped;
+    let contents = render post in
+    Ops.create_file ops ~path ~contents
+end
 
 let compare_date a b =
   let pa = Ptime.of_date a |> Option.value_exn in
   let pb = Ptime.of_date b |> Option.value_exn in
   Ptime.compare pa pb
-
-let date_to_rss_string date =
-  let ptime = Ptime.of_date date |> Option.value_exn in
-  let y, m, d = date in
-  let weekday = Ptime.weekday ptime in
-  Printf.sprintf "%s, %d %s %d 00:00:00 UT"
-    (weekday_to_string weekday)
-    d (month_name m) y
-
-let date_to_short_string date =
-  let y, m, d = date in
-  Printf.sprintf "%d %s %d" d (month_name m) y
-
-let post_to_rss_item (post : post) ~config =
-  let open Tyxml.Xml in
-  node "item"
-    [
-      node "title" [ pcdata post.title ];
-      node "link" [ pcdata (post_permalink post) ];
-      node "description" [ cdata (post_rss_description post) ];
-      node "pubDate" [ pcdata (date_to_rss_string (post_pubdate post)) ];
-      node "guid" [ pcdata (post_permalink post) ];
-      node "dc:creator" [ pcdata config.author_name ];
-    ]
 
 let rss_feed posts config =
   let open Tyxml.Xml in
@@ -161,9 +208,9 @@ let rss_feed posts config =
       (let path = Uri.path config.root in
        path // "rss.xml")
   in
-  let items = List.map posts ~f:(post_to_rss_item ~config) in
+  let items = List.map posts ~f:(Post.to_rss_item ~config) in
   let oldest_date =
-    List.map posts ~f:post_pubdate
+    List.map posts ~f:Post.pub_date
     |> List.min_elt ~compare:compare_date
     |> Option.value_exn
   in
@@ -211,35 +258,11 @@ let feed_config =
     root = Uri.of_string "http://blog.emillon.org";
   }
 
-let with_string_formatter f =
-  let buf = Buffer.create 0 in
-  let fmt = Stdlib.Format.formatter_of_buffer buf in
-  f fmt;
-  Buffer.contents buf
-
 let tag_href tag = "tags" // Printf.sprintf "%s.html" tag
 
 module Templates = struct
-  let render input l =
-    let env = Map.of_alist_exn (module String) l in
-    with_string_formatter (fun fmt -> Template.process env ~input ~output:fmt)
-
-  let default ~title ~body =
-    render "templates/default.html" [ ("title", title); ("body", body) ]
-
-  let post (post : post) =
-    let title = post.title in
-    let body =
-      render "templates/post.html"
-        [
-          ("title", title);
-          ("author", post.author);
-          ("date", date_to_short_string (post_pubdate post));
-          ("prettytags", String.concat ~sep:", " (Set.to_list post.tags));
-          ("body", Cmarkit_html.of_doc ~safe:true post.body);
-        ]
-    in
-    default ~title ~body
+  (* TODO: inline *)
+  let render = render_template
 
   let post_items posts =
     with_string_formatter (fun fmt ->
@@ -248,8 +271,8 @@ module Templates = struct
               Map.of_alist_exn
                 (module String)
                 [
-                  ("date", date_to_rss_string (post_pubdate post));
-                  ("url", post_permalink post);
+                  ("date", date_to_rss_string (Post.pub_date post));
+                  ("url", Post.permalink post);
                   ("title", post.title);
                 ]
             in
@@ -264,7 +287,7 @@ module Templates = struct
           ("feed", Uri.to_string feed);
         ]
     in
-    default ~body ~title
+    render_default ~body ~title
 
   let index posts ~tags =
     let tag_cloud =
@@ -296,7 +319,7 @@ module Templates = struct
       render "templates/index.html"
         [ ("posts", post_items posts); ("tagcloud", tag_cloud) ]
     in
-    default ~body ~title:"Home"
+    render_default ~body ~title:"Home"
 end
 
 let create_tag_html ops tag posts =
@@ -313,26 +336,11 @@ let create_tag_feed ops tag posts =
   let contents = rss_feed posts feed_config in
   Ops.create_file ops ~path ~contents
 
-let directory_exists path =
-  Stdlib.Sys.file_exists path && Stdlib.Sys.is_directory path
-
-let create_post_assets ops path =
-  Ops.mkdir ops path;
-  List.iter (readdir path) ~f:(fun path ->
-      Ops.create_file ops ~path ~contents:(In_channel.read_all path))
-
-let create_post ops post =
-  let chopped = "posts" // post.basename |> Stdlib.Filename.chop_extension in
-  let path = chopped ^ ".html" in
-  if directory_exists chopped then create_post_assets ops chopped;
-  let contents = Templates.post post in
-  Ops.create_file ops ~path ~contents
-
 let run ~input ~output =
   let ops =
     match output with None -> Ops.dry_run | Some root -> Ops.real ~root
   in
-  let posts = load_all_posts (input // "posts") in
+  let posts = Post.load_all (input // "posts") in
   let tag_map =
     List.fold posts
       ~init:(Map.empty (module String))
@@ -344,7 +352,7 @@ let run ~input ~output =
   let rss_feed = rss_feed posts feed_config in
   Ops.mkdir ops ".";
   Ops.mkdir ops "posts";
-  List.iter posts ~f:(create_post ops);
+  List.iter posts ~f:(Post.create ops);
   Ops.mkdir ops "tags";
   Ops.mkdir ops "feeds";
   List.iter all_tags ~f:(fun tag ->
