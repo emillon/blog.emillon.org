eab696661351d2c9109c02e1ea10e2ece5cf13b6
Etienne Millon <me@emillon.org>
2025-05-21 14:29:19 +0200

    Remove custom engine

--- flake.nix
+++ flake.nix
@@ -7,36 +7,9 @@
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
-        packageName = "blog";
-        newDeps = with pkgs.ocamlPackages; [
-          pkgs.html-tidy
-          pkgs.libxml2
-          base
-          cmarkit
-          cmdliner
-          ptime
-          stdio
-          tyxml
-          uri
-          yaml
-        ];
-        blogEngine = pkgs.haskellPackages.callCabal2nix "blog" self rec { };
-        blogEngineNew = pkgs.ocamlPackages.buildDunePackage {
-          version = "n/a";
-          src = ./.;
-          pname = "blog";
-          propagatedBuildInputs = newDeps;
-        };
-        haskellPackages = pkgs.haskellPackages;
       in
       {
         formatter = pkgs.nixpkgs-fmt;
-        packages.new = blogEngineNew;
-        devShells.new = pkgs.mkShell {
-          buildInputs = newDeps
-            ++ (with pkgs.ocamlPackages; [ merlin ocamlformat_0_26_2 ]);
-          inputsFrom = [ blogEngineNew ];
-        };
-        packages.default = blogEngine;
+        packages.default = pkgs.haskellPackages.callCabal2nix "blog" self { };
       });
 }
